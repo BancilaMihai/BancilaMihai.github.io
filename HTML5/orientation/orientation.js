fa2f0b34e35924166845495e7c2155f4298f79e5
BancilaMihai <mihai.bancila@yahoo.com>
2018-10-30 13:53:41 +0300

    update version

--- HTML5/orientation/orientation.js
+++ HTML5/orientation/orientation.js
@@ -9,6 +9,7 @@ function on_device_orientation(e){
 	var canvas=document.getElementById("id_canvas");
 	var context=canvas.getContext("2d");
 
+	context.clearRect(0,0,canvas)
 	var R=10;
 	context.beginPath();
 	context.arc(canvas.width/2+e.gamma/90*(canvas.width/2-R),
