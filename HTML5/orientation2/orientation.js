a961eab4b911e54017ba293cb38d57f80d5eb623
BancilaMihai <mihai.bancila@yahoo.com>
2018-11-05 13:41:31 +0200

    qwer

--- HTML5/orientation2/orientation.js
+++ HTML5/orientation2/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic version: 2018.11.05.0";
+document.getElementById("id_logic_version").innerHTML="Logic version: 2018.11.05.2";
 window.addEventListener("devicemotion", on_device_motion)
 
 function on_device_orientation(e){
@@ -16,7 +16,7 @@ function on_device_orientation(e){
 				canvas.height/2+e.beta/90*(canvas.height/2-R),
 				R,0,2*Math.PI);
 	context.stroke();
-	
+
 }
 function on_device_motion(e){
 	var acc_z = e.accelerationIncludingGravity.z;
@@ -26,4 +26,8 @@ function on_device_motion(e){
 	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z * 100) / 100;
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
+
+	document.getElementById("id_rot_x").innerHTML = Math.round(Math.atan(acc_x/acc_z)*100)/100;
+	document.getElementById("id_rot_y").innerHTML = Math.round(Math.atan(acc_y/acc_z)*100)/100;
+
 }
