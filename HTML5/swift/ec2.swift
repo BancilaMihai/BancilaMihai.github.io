a8fa8b7c2108769eeb436bbf468ceaef4e9bfb6f
BancilaMihai <mihai.bancila@yahoo.com>
2018-11-19 17:54:11 +0200

    swift

--- HTML5/swift/ec2.swift
+++ HTML5/swift/ec2.swift
@@ -1,89 +1,60 @@
 import Foundation
 
-func citeste(a: inout Float,b: inout Float,c: inout Float){ //a: inout Float-referinta-valorile modif se transmit in afara, a:Float -asa nu era bine, era prin copiere
-    print("a= ")
-    let s_opt=readLine()
-    if let s=s_opt{
-        let a_opt=Float(s)
-        if let _a=a_opt{
-        //il am pe a
-            print("b= ")
-            let s_opt=readLine()
-            if let s=s_opt{
-                let a_opt=Float(s)
-                if let _b=a_opt{
-                //il am pe b
-                    print("c= ")
-                    let s_opt=readLine()
-                    if let s=s_opt{
-                        let a_opt=Float(s)
-                        if let _c=a_opt{
-                            //il am pe c
-                            a=_a
-                            b=_b
-                            c=_c
-                        }else{
-                        a=1
-                        b=2
-                        c=1
+print("a=")
+var s = readLine()
+if let sa = s{
+    let x = Float(sa)
+    if let a = x{
+        print("b=")
+        let s = readLine()
+        if let sa = s{
+            let x = Float(sa)
+            if let b = x{
+                print("c=")
+                let s = readLine()
+                if let sa = s{
+                    let x = Float(sa)
+                    if let c = x{
+                        print("a = \(a), b = \(b), c= \(c)")
+                        
+                        let delta = b * b - 4 * a * c
+                        var x1_re, x2_re, x1_im, x2_im: Float
+                        if delta >= 0{
+                            x1_re = (-b - sqrt(delta)) / (2 * a)
+                            x2_re = (-b + sqrt(delta)) / (2 * a)
+                            x1_im = 0
+                            x2_im = 0
                         }
-                    }else{
-                    a=1
-                    b=2
-                    c=1
+                        else{
+                            x1_re = (-b) / (2 * a) 
+                            x2_re = (-b) / (2 * a)
+                            x1_im = -sqrt(-delta) / (2 * a)
+                            x2_im = sqrt(-delta) / (2 * a)
+                        }
+                        print("x1 = \(x1_re) + \(x1_im)")
+                        print("x2 = \(x2_re) + \(x2_im)")
                     }
-                }else{
-                a=1
-                b=2
-                c=1
+                    else{
+                        print("Invalid conversion")
+                    }
                 }
-            }else{
-            a=1
-            b=2
-            c=1
+                else{
+                    print("Empty string")
+                }        
+                
             }
-        }else{
-        a=1
-        b=2
-        c=1
+            else{
+                print("Invalid conversion")
+            }
         }
-    }else{
-    a=1
-    b=2
-    c=1
+        else{
+            print("Empty string")
+        }        
+    }
+    else{
+        print("Invalid conversion")
     }
 }
-func calculeaza(a:Float,b:Float,c:Float,x1_re:inout Float,x1_im:inout Float,x2_re:inout Float,x2_im:inout Float){
-    print("a=\(a),b=\(b),c=\(c)")
-    let delta=b*b-4*a*c
-    if delta>=0{
-        x1_re=(-b-sqrt(delta))/(2*a)
-        x2_re=(-b+sqrt(delta))/(2*a)
-        x1_im=0
-        x2_im=0
-    }else{
-        x1_re=(-b)/(2*a)
-        x1_im=(-sqrt(delta))/(2*a)
-        x2_re=(-b)/(2*a)
-        x2_im=(sqrt(delta))/(2*a)
-    }
+else{
+    print("Empty string")
 }
-func tiparire(x1_re:Float,x1_im:Float,x2_re:Float,x2_im:Float){
-    print("x1=\(x1_re)+\(x1_im)")
-    print("x2=\(x2_re)+\(x2_im)") 
-}
-
-var a,b,c:Float //var a b c de tip float
-a=0
-b=0
-c=0
-var x1_re,x2_re,x1_im,x2_im:Float
-x1_re=0
-x2_re=0
-x1_im=0
-x2_im=0
-citeste(a:&a,b:&b,c:&c)//la a ii dam valoarea variabilei a -&a s-a transmis prin referinta
-calculeaza(a:a,b:b,c:c,x1_re:&x1_re,x1_im:&x1_im,x2_re:&x2_re,x2_im:&x2_im)
-tiparire(x1_re:x1_re,x1_im:x1_im,x2_re:x2_re,x2_im:x2_im)
-
-
