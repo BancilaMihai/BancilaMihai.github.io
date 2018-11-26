7049b9984bb741c80828b6354f7874213f47eed5
BancilaMihai <mihai.bancila@yahoo.com>
2018-11-26 15:47:34 +0200

    update 12

--- HTML5/touch/touch.js
+++ HTML5/touch/touch.js
@@ -1,6 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.11";
-
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.8";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.12";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);//lista degetelor care ating ecranul
 canvas.addEventListener("touchmove", on_touch_move);
@@ -8,30 +6,39 @@ canvas.addEventListener("touchend", on_touch_end);//lista degetelor care s-au ri
 
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
+
 var last_pos_array = [];
 
+function get_random_color(e){
+	var litere = '0123456789ABCDEF';
+	var s = "#FFFFFF";
+	for(var i = 0;i < 6;i++)
+		s = s + litere[Math.floor(Math.random() * 16)]
+}
 function on_touch_start(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		
 		var last_pos = {x: e.changedTouches[i].pageX, 
 						y: e.changedTouches[i].pageY, 
 						id: e.changedTouches[i].identifier,
 						color: get_random_color()};
 		last_pos_array.push(last_pos);
-		
-		context.fillStyle = last_pos_array[last_pos_array - 1].color;
-		context.strokeStyle = last_pos_array[last_pos_array - 1].color;
+
+		context.fillStyle = last_pos_array[last_pos_array.length - 1].color;
+		context.strokeStyle = last_pos_array[last_pos_array.length - 1].color;
 		
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0, 2 * Math.PI);
+		context.fill();
 		context.stroke();
+
 	}
 }
+
 function on_touch_move(e)
 {
 	e.preventDefault();
@@ -46,20 +53,23 @@ function on_touch_move(e)
 		context.beginPath();
 		context.lineWidth = 20;
 		//calculam mijlocul cercului anterior
+		context.fillStyle = last_pos_array[j].color;
+		context.strokeStyle = last_pos_array[j].color;
 		context.moveTo(last_pos_array[j].x - canvas_bounding_rect.left, last_pos_array[j].y - canvas_bounding_rect.top);
 		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 					e.changedTouches[i].pageY - canvas_bounding_rect.top);
 		context.stroke();
 		
 		context.beginPath();
-		context.lineWidth = 1;
+		context.lineWidth = 1;		
 		context.fillStyle = last_pos_array[j].color;
 		context.strokeStyle = last_pos_array[j].color;
-		
+
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0, 2 * Math.PI);
+		context.fill();
 		context.stroke();
 		//actializam ultima pozitie
 		last_pos_array[j].x = e.changedTouches[i].pageX;
@@ -67,6 +77,7 @@ function on_touch_move(e)
 	}
 	
 }
+
 function on_touch_end(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++){
