90401c40ec3dcc4572b8cfd2a76245fc0d0d4298
BancilaMihai <mihai.bancila@yahoo.com>
2018-11-26 15:25:43 +0200

    qwerty

--- HTML5/touch/touch.js
+++ HTML5/touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.7";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.8";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);//lista degetelor care ating ecranul
