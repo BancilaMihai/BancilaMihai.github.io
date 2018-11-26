4a78fd3aeb6ddf3abc39d72e93c2b25610ffbc2d
BancilaMihai <mihai.bancila@yahoo.com>
2018-11-26 14:45:17 +0200

    new update 6

--- HTML5/touch/touch.js
+++ HTML5/touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.5";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.6";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
@@ -25,11 +25,13 @@ function on_touch_move(e){
 		for(var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.lineWith = 20;
 		//calculam mijlocul cercului anterior
 		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
 		context.lineTo(e.changedTouches[i].pageX-canvas_bounding_rect.left,
 					e.changedTouches[i].pageY-canvas_bounding_rect.top);
-					
+		context.stroke();
+		context.lineWith = 1;
 		context.arc(e.changedTouches[i].pageX-canvas_bounding_rect.left,
 					e.changedTouches[i].pageY-canvas_bounding_rect.top,
 					10,
