7a7b5be8068e8a3f4200b999de8bd945b6178911
BancilaMihai <mihai.bancila@yahoo.com>
2019-01-20 18:33:31 +0200

    update_nivela

--- HTML5/convertor/nivela.js
+++ HTML5/convertor/nivela.js
@@ -1,4 +1,5 @@
-document.getElementById("id_nivela").innerHTML="Logic version nivela: 2019.01.20.1";
+document.getElementById("id_nivela").innerHTML="Logic version nivela: 2019.01.20.2";
+window.addEventListener("deviceorientation", on_device_orientation)
 
 function on_device_orientation(e){
 	document.getElementById("id_alpha").innerHTML=e.alpha;
