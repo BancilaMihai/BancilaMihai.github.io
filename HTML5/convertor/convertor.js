9ddcb523242e53c572e373a3539eb01430bc3d32
BancilaMihai <mihai.bancila@yahoo.com>
2019-01-20 11:37:23 +0200

    update_masiv

--- HTML5/convertor/convertor.js
+++ HTML5/convertor/convertor.js
@@ -1,1 +1,9 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2019.01.19.1";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2019.01.19.2";
+
+function dinKginGr(valoareKg) {
+  document.getElementById("rezultatGr").innerHTML=valoareKg*1000;
+}
+
+function dinGrinKg(valoareGr) {
+  document.getElementById("rezultatKg").innerHTML=valoareGr/1000;
+}
--- HTML5/convertor/data_curenta.js
+++ HTML5/convertor/data_curenta.js
@@ -1,1 +1,30 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2019.01.19.101";
+var data_locala = new Date();
+  var zi = new Array(7);
+  
+  zi[1] = "Luni";
+  zi[2] = "Marti";
+  zi[3] = "Miercuri";
+  zi[4] = "Joi";
+  zi[5] = "Vineri";
+  zi[6] = "Sambata";
+  zi[0] = "Duminica";
+
+  var data_c = zi[data_locala.getDay()];
+document.getElementById("id_data_curenta").innerHTML = data_c;
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("id_ora_curenta").innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
