b91d459f211ac6ec8844d6c1318e7e90c39d8bc4
BancilaMihai <mihai.bancila@yahoo.com>
2019-01-20 18:55:18 +0200

    update_nivela

--- HTML5/convertor/nivela.js
+++ HTML5/convertor/nivela.js
@@ -1,4 +1,4 @@
-document.getElementById("id_nivela").innerHTML="Logic version nivela: 2019.01.20.3";
+document.getElementById("id_nivela").innerHTML="Logic version nivela: 2019.01.20.4";
 window.addEventListener("deviceorientation", on_device_orientation)
 
 function rotunjire(numar){
@@ -7,5 +7,5 @@ return Math.round(numar*100)/100
 
 function on_device_orientation(e){
 	document.getElementById("id_beta").innerHTML = rotunjire(e.beta);
-	setTimeout(myFunction, 5000);
+	setTimeout(on_device_orientation, 5000);
 }
